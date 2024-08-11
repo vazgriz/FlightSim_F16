5416c2010820ebcbda077c20411ff6706a232b33
vazgriz <contact@ryan-vazquez.dev>
2024-08-11 18:40:25 -0500

    Add named constants to AirDataComputer

--- Assets/Scripts/F16/AirDataComputer.cs
+++ Assets/Scripts/F16/AirDataComputer.cs
@@ -21,16 +21,24 @@ public class AirDataComputer {
     /// <param name="altitude">Altitude in ft</param>
     /// <returns>Air data</returns>
     public AirData CalculateAirData(float velocity, float altitude) {
+        const float baseTemperature = 519.0f;        // sea level temp in R
+        const float minTemperature = 390.0f;         // minimum temp in R
+        const float temperatureGradient = 0.703e-5f; // gradient in R / ft
+
+        const float gamma = 1.4f; // ratio of specific heats
+        const float gasConstant = 1716.3f;
+        const float densityPower = 4.14f;
+
         altitude = Mathf.Clamp(altitude, 0, MaxAltitude);
-        float temperatureFactor = 1.0f - 0.703e-5f * altitude;
-        float T = 519.0f * temperatureFactor;   // temperature in Rankine
 
-        if (altitude >= MaxAltitude) {
-            T = 390.0f;
-        }
+        // calculate temperature in Rankine
+        float temperatureFactor = 1.0f - (temperatureGradient * altitude);
+        float T = Mathf.Max(minTemperature, baseTemperature * temperatureFactor);
 
-        float rho = SeaLevelDensity * Mathf.Pow(temperatureFactor, 4.14f);
-        float altitudeMach = velocity / Mathf.Sqrt(1.4f * 1716.3f * T);
+        float speedOfSound = Mathf.Sqrt(gamma * gasConstant * T);
+        float altitudeMach = velocity / speedOfSound;
+
+        float rho = SeaLevelDensity * Mathf.Pow(temperatureFactor, densityPower);
         float qBar = 0.5f * rho * velocity * velocity;
 
         return new AirData() {
